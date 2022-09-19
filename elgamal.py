08bb3a07e8fbd51781433496e8aecad0af2d8590
tanner-nordell <hnordell@seas.upenn.edu>
2022-09-18 23:04:28 -0400

    push number 4. sunday night

--- elgamal.py
+++ elgamal.py
@@ -4,18 +4,18 @@ from params import p
 from params import g
 
 def keygen():
-    sk = random.SystemRandom().randint(1, pow(2,2048))
+    sk = random.SystemRandom().randint(1, pow(2, 2048))
     pk = pow(g, sk, p)
-    return pk,sk
+    return pk, sk
 
 def encrypt(pk,m):
-    q = (p-1)/2;
+    q = (p-1)/2
     r = random.SystemRandom().randint(1, q)
     c1 = pow(g, r, p)
-    c2 = (pow(pk, r, p )* (m%p)) % p
-    return [c1,c2]
+    c2 = (pow(pk, r, p) * (m % p)) % p
+    return [c1, c2]
 
-def decrypt(sk, c):
-    m = c[1]/(c[0]**sk)%p
+def decrypt (sk, c):
+    m = (c[1]%p) / (pow(c[0], sk, p))
     return m
 
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+from elgamal import keygen, encrypt, decrypt
 
+if __name__ == '__main__':
+    keys = keygen()
+    print(keys)
+    c = encrypt(keys[0], 10000001)
+    print(c)
+    print(decrypt(keys[1], c))
